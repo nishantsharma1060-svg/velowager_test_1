3e6282f8c60e31544d49e91caa8aa7b5510f2e69
Nishant <you@example.com>
2026-07-06 00:25:36 +0530

    Fix Render deployment

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,6 +6,7 @@ import * as schema from './schema.ts';
 // Configure the database connection pool parameters
 const poolConfig = {
   host: process.env.SQL_HOST,
+  port: Number(process.env.SQL_PORT || 5432),
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
   database: process.env.SQL_DB_NAME,
